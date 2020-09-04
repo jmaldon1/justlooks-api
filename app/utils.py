ca93c8f25a256ae7c762784134d649b2eec76272
Joshua Maldonado <joshjm9915@gmail.com>
2020-09-04 11:10:43 -0400

    func to append query params to url

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,7 @@
 """Utility functions used throughout the project
 """
 import os
+from urllib.parse import urlparse, parse_qsl, urlencode, urlunparse
 
 
 def read_file(path: str) -> str:
@@ -30,3 +31,22 @@ def replace_single_len_lists(request_params: dict) -> dict:
         dict: request params with single length lists replaced
     """
     return {key: val[0] if len(val) == 1 else val for key, val in request_params.items()}
+
+
+def add_query_params_to_url(url: str, query_params: dict) -> str:
+    """It takes this many lines of python to add query params to a URL for some reason.
+
+    Args:
+        url (str): URL.
+        query_params (dict): Query parameters.
+
+    Returns:
+        str: URL with query parameters added.
+    """
+    parsed_url = urlparse(url)
+    query = parsed_url.query
+    url_dict = dict(parse_qsl(query))
+    url_dict.update(query_params)
+    url_new_query = urlencode(url_dict)
+    url_parse = parsed_url._replace(query=url_new_query)
+    return urlunparse(url_parse)
